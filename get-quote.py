136f6dad3583f65f8ed52bdb0ede1965f0f739cc
garnerkaty90 <71801874+garnerkaty90@users.noreply.github.com>
2020-09-26 13:31:41 -0400

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,10 @@ def primary():
   quotes = f.readlines()
   f.close()
 
-  last = 13
+  last = 15
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  print(quotes[14])
 
 if __name__== "__main__":
   primary()
